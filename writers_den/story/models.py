a413c4cbb52c35e182adee7dcbadb1bf18ca8ddb
Kishan Mehta <kishan@mobifly.co.uk>
2019-03-14 14:19:19 +0530

    Adds two apps at backend for signup and stories.

--- writers_den/story/models.py
+++ writers_den/story/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-from writers_den.user.models import User
 
 
 class Story(models.Model):
@@ -15,5 +14,3 @@ class Story(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=DRAFT)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    
